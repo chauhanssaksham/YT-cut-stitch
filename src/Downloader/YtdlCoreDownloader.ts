8ff69f1c6f7ab0d7ccaa9100c663dd1ab2cec855
Saksham Chauhan <saksham@Sakshams-MacBook-Pro.local>
2024-06-14 11:32:22 +0530

    Improved quality format check

--- src/Downloader/YtdlCoreDownloader.ts
+++ src/Downloader/YtdlCoreDownloader.ts
@@ -67,11 +67,16 @@ export class YtdlCoreDownloader implements IVideoDownloader{
         for (const format of formats) {
             // Prioritize formats with both video and audio
             if (format.hasVideo && format.hasAudio) {
-                if (!highestQualityFormat || format.qualityLabel > highestQualityFormat.qualityLabel)  {
+                if (!highestQualityFormat || 
+                    this.qualityLabelsSorted.indexOf(format.qualityLabel) > 
+                    this.qualityLabelsSorted.indexOf(highestQualityFormat.qualityLabel))  {
                     highestQualityFormat = format;
                 }
             }
         }
         return highestQualityFormat;
     }
+    private qualityLabelsSorted = ['144p', '144p 15fps', '144p60 HDR', '240p', '240p60 HDR', '270p', '360p', '360p60 HDR'
+    , '480p', '480p60 HDR', '720p', '720p60', '720p60 HDR', '1080p', '1080p60', '1080p60 HDR', '1440p'
+    , '1440p60', '1440p60 HDR', '2160p', '2160p60', '2160p60 HDR', '4320p', '4320p60'];
 };
